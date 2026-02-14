43e8d71b5e5ce207c49a0ea17ea56c17f6dd2c8e
shanmuk-coder <chinthamneedisanschakradhar@gmail.com>
2026-02-14 15:50:06 +0530

    Add program to calculate the sum of two numbers
    Implement a program to calculate the sum of two user-input numbers.

---
+++ sum_of_two_numbers.java
@@ -0,0 +1,15 @@
+// WRITE A PROGRAM TO DISPLAY THE SUM OF TWO NUMBERS 
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+      //create a scanner object to take input from the user.
+         Scanner sc=new Scanner(System.in);
+        System.out.println("ENTER THE VALUE OF A :");
+        int a=sc.nextInt(); 
+        System.out.println("ENTER THE VALUE OF B :");
+        int b=sc.nextInt();
+        int sum=a+b;  //CALCULATE THE SUM OF TWO NUMBERS
+        System.out.println("THE SUM OF TWO NUMBERS:" +sum);
+        sc.close();
+    }
+}
