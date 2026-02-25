3893a278e1e35884965dbb380e56cc1dd402f8f2
shanmuk-coder <chinthamneedisanschakradhar@gmail.com>
2026-02-25 22:26:42 +0530

    Add SquareRoot.java to calculate square roots
    Implement a Java program to calculate the square root of a number.

---
+++ SquareRoot.java
@@ -0,0 +1,9 @@
+//write a java program to calculate the square root of a number.
+import java.util.Scanner;
+class SquareRoot{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println((int)Math.sqrt(n));
+    }
+}
