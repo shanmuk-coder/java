60a18569a7dac3617c129c4722ea8969e345bd93
shanmuk-coder <chinthamneedisanschakradhar@gmail.com>
2026-03-12 20:51:41 +0530

    Add program to find the sum of all digits of a number

---
+++ Sum_Of_Digits.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Sum_Of_Digits{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int n=sc.nextInt();
+        int sum=0;
+        while(n!=0)
+        {
+            int digit=n%10;
+            sum=sum+digit;
+            n=n/10;
+        }
+        System.out.println("Sum of Digits in a number:" +sum);
+    }
+}
