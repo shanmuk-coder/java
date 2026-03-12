bf8477fc31becfdf31a6929b32a1e4abce2acfd8
shanmuk-coder <chinthamneedisanschakradhar@gmail.com>
2026-03-12 20:39:27 +0530

    Add program to reverse an array.

---
+++ Reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Reverse{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n=sc.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Reverse order:");
+        for(int j=arr.length-1;j>=0;j--)
+        {
+            System.out.print(arr[j] +" ");
+        }
+    }
+}
