4f0eb67d5166a65c6edcd62c514813115de82d34
shanmuk-coder <chinthamneedisanschakradhar@gmail.com>
2026-03-14 21:53:47 +0530

    Add program to count frequency of a number.

---
+++ ElementFrequency.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+class ElementFrequency{
+    public static void main(String[] args) {
+   Scanner sc=new Scanner(System.in);
+   System.out.println("Enter the size of Array:");
+   int n=sc.nextInt();
+   int [] arr=new int[n];
+   int count=0;
+   System.out.println("Enter Elements:");
+  for(int i=0;i<n;i++)
+  {
+      arr[i]=sc.nextInt();
+  }
+System.out.println("Enter the x value:");
+  int x=sc.nextInt();
+  for(int j=0;j<arr.length;j++)
+  {
+      if(arr[j]==x)
+      {
+          count++;
+      }
+  }
+  System.out.println(count);
+    }
+}
